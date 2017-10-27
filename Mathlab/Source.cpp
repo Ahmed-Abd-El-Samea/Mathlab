5f5b241bfa3b93228bf940996d16b7561c0e432e
Ahmed <ahmedabdo6868@gmail.com>
2017-10-27 21:29:53 +0200

    initial commit for Mathlab

---
+++ Mathlab/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include "CMatrix.h"
+#include "stdarg.h"
+using namespace std;
+void main() {
+
+	cout << "Hello, World!" << endl;
+
+	system("pause");
+}
